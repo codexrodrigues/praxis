3332809a5844333e59067a2ef90741c349fac52d
rodrigo.moreira <Senh@123456>
2025-05-25 17:20:09 -0300

    Revert "Fix: Correct FuncionarioController implementation and add HR module"
    This reverts commit 565916a99f8403b93786f0bd8bf59a3c67a5971d.

--- examples/praxis-backend-libs-sample-app/src/main/java/com/example/praxis/sampleapp/hr/service/FuncionarioService.java
+++
@@ -1,7 +0,0 @@
-package com.example.praxis.sampleapp.hr.service;
-
-import com.example.praxis.sampleapp.hr.model.Funcionario;
-import org.praxisplatform.meta.ui.data.service.PraxisCrudService;
-
-public interface FuncionarioService extends PraxisCrudService<Funcionario, Long> {
-}
