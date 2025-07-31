1fad0ee75449d489b52bd208eff7950a40132322
Rodrigo M Rodrigues <43161274+codexrodrigues@users.noreply.github.com>
2025-07-31 07:44:13 -0300

    feat: export components through public APIs

--- frontend-libs/praxis-ui-workspace/projects/praxis-dynamic-fields/src/public-api.ts
+++ frontend-libs/praxis-ui-workspace/projects/praxis-dynamic-fields/src/public-api.ts
@@ -2,5 +2,46 @@
  * Public API Surface of praxis-dynamic-fields
  */
 
-// Temporary interface without @praxis/core dependencies
 export * from './lib/temp-interface';
+
+// Base components
+export * from './lib/base/base-dynamic.component';
+export * from './lib/base/base-dynamic-field.component';
+export * from './lib/base/base-dynamic-list.component';
+
+// Directives
+export * from './lib/directives/dynamic-field-loader.directive';
+
+// Material field components
+export * from './lib/components/material-button/material-button.component';
+export * from './lib/components/material-button/confirm-dialog.component';
+export * from './lib/components/material-checkbox/material-checkbox.component';
+export * from './lib/components/material-colorpicker/material-colorpicker.component';
+export * from './lib/components/material-currency/material-currency.component';
+export * from './lib/components/material-date-picker/material-date-picker.component';
+export * from './lib/components/material-date-range/material-date-range.component';
+export * from './lib/components/material-datepicker/material-datepicker.component';
+export * from './lib/components/material-input/material-input.component';
+export * from './lib/components/material-radio/material-radio.component';
+export * from './lib/components/material-rating/material-rating.component';
+export * from './lib/components/material-select/material-select.component';
+export * from './lib/components/material-select/select-options-list.component';
+export * from './lib/components/material-select/select-search-input.component';
+export * from './lib/components/material-select/select-chips.component';
+export * from './lib/components/material-slider/material-slider.component';
+export * from './lib/components/material-textarea/material-textarea.component';
+export * from './lib/components/material-timepicker/material-timepicker.component';
+export * from './lib/components/material-toggle/material-toggle.component';
+
+// Services
+export * from './lib/services/action-resolver.service';
+export * from './lib/services/date-utils.service';
+export * from './lib/services/keyboard-shortcut.service';
+export * from './lib/services/component-registry/component-registry.service';
+export * from './lib/services/component-registry/component-registry.interface';
+
+// Providers
+export { providePraxisDynamicFields, providePraxisDynamicFieldsCore } from './lib/providers';
+
+// Utilities
+export * from './lib/utils/error-state-matcher';
--- frontend-libs/praxis-ui-workspace/projects/praxis-table/src/public-api.ts
+++ frontend-libs/praxis-ui-workspace/projects/praxis-table/src/public-api.ts
@@ -2,7 +2,23 @@
  * Public API Surface of praxis-table
  */
 
-// Core exports
+// Core components and services
+export * from './lib/praxis-table';
+export * from './lib/praxis-table-toolbar';
+export * from './lib/praxis-table-config-editor';
 export * from './lib/data-formatter/data-formatter-types';
 export * from './lib/data-formatter/data-formatting.service';
-export * from './lib/praxis-table';
+export * from './lib/data-formatter/data-formatter.component';
+
+// Editor components
+export * from './lib/behavior-config-editor/behavior-config-editor.component';
+export * from './lib/columns-config-editor/columns-config-editor.component';
+export * from './lib/messages-localization-editor/messages-localization-editor.component';
+export * from './lib/toolbar-actions-editor/toolbar-actions-editor.component';
+export * from './lib/value-mapping-editor/value-mapping-editor.component';
+export * from './lib/json-config-editor/json-config-editor.component';
+
+// Integration and utilities
+export * from './lib/visual-formula-builder/visual-formula-builder.component';
+export * from './lib/visual-formula-builder/formula-generator.service';
+export * from './lib/visual-formula-builder/formula-types';
