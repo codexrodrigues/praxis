4fea42b072c01368fa6e51e72c8f0062cfd49fe4
Rodrigo M Rodrigues <43161274+codexrodrigues@users.noreply.github.com>
2025-08-09 23:39:21 -0300

    fix(dynamic-fields): render MatSelect mode through template

--- frontend-libs/praxis-ui-workspace/projects/praxis-dynamic-fields/src/lib/components/material-select/material-select.component.ts
+++ frontend-libs/praxis-ui-workspace/projects/praxis-dynamic-fields/src/lib/components/material-select/material-select.component.ts
@@ -27,7 +27,6 @@ import {
     >
       <mat-label>{{ metadata()?.label || 'Select' }}</mat-label>
       <mat-select
-        [multiple]="multiple()"
         [formControl]="internalControl"
         [placeholder]="metadata()?.placeholder || ''"
         [required]="metadata()?.required || false"
